5ad55daf0f1c8ef3ffdcbe451ff763c8644486c2
Lucas Rocha <lucas@gigage.dev>
2020-08-25 12:26:30 -0300

    refactor: move all error imports

--- src/presentation/controllers/singup.spec.ts
+++ src/presentation/controllers/singup.spec.ts
@@ -1,8 +1,6 @@
 import SignUpController from './SignUpController'
-import { MissingParamError } from '../errors/missing-param-error'
-import { InvalidParamError } from '../errors/invalid-param-error'
 import { EmailValidator } from '../interfaces/email-validator'
-import { ServerError } from '../errors/server-error'
+import { MissingParamError, InvalidParamError, ServerError } from '../errors/'
 interface sutTypes {
   sut: SignUpController
   emailValidatorStub: EmailValidator
