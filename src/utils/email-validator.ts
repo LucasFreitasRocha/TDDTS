c55c16b4ba259dbde1af7039ced80c5de8d9ae01
Lucas Rocha <lucas@gigage.dev>
2020-09-21 21:49:44 -0300

    feat: ensure EmailValidatorAdapter returns false if validator returns false

---
+++ src/utils/email-validator.ts
@@ -0,0 +1,6 @@
+import { EmailValidator } from '../presentation/interfaces/email-validator'
+export class EmailValidatorAdapter implements EmailValidator {
+  isValid (email: string): boolean {
+    return false
+  }
+}
