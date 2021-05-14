75d209f48162bfa3b04e618b866d021078b157ac
lucas.rocha <lucas.rocha@ucam-campos.br>
2021-05-14 18:25:24 -0300

    feat: ensure cors is enabled

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -1,5 +1,7 @@
+import { cors } from './../middlewares/cors'
 import { bodyParser } from './../middlewares/body-parser'
 import { Express } from 'express'
 export default (app: Express): void => {
   app.use(bodyParser)
+  app.use(cors)
 }
