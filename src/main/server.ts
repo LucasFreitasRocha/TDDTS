270aa8c74f60dcad25dd87b089311ba0131902c8
lucas.rocha <lucas.rocha@ucam-campos.br>
2021-05-15 09:26:14 -0300

    feat: connect to mongodb before app listen

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,2 +1,10 @@
-import app from './config/app'
-app.listen(5759, () => console.log('server started at http://localhost:5759'))
+
+import { MongoHelper } from '../infra/db/mongodb/helpers/mongo-helper'
+import env from './config/env'
+
+MongoHelper.connect(env.mongoUrl)
+  .then(async () => {
+    const app = (await import ('./config/app')).default
+    app.listen(env.port, () => console.log(`server started at http://localhost:${env.port}`))
+  })
+  .catch(console.error)
